4988f4b45d2ff0441925450e50bf5ee1b460fd45
unknown <azazy.ehab@gmail.com>
2022-09-14 07:47:55 +0200

    New Task

---
+++ Employee.cs
@@ -0,0 +1,43 @@
+﻿namespace ObjectsCompare
+{
+    class Employee
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public string Department { get; set; }
+        public static bool operator ==(Employee Lhand, Employee Rhand)
+        {
+            return Lhand.Equals(Rhand);
+        }
+        public static bool operator !=(Employee Lhand, Employee Rhand)
+        {
+            return !(Lhand.Equals(Rhand));
+        }
+
+        #region Overrided Funcs
+        public override bool Equals(object obj)
+        {
+            if ((obj is null) || !(obj is Employee))
+                return false;
+            var emp = obj as Employee;
+            return
+                this.ID == emp.ID &&
+                this.Name == emp.Name &&
+                this.Salary == emp.Salary &&
+                this.Department == emp.Department;
+        }
+        public override int GetHashCode()
+        {
+            // We used Prime Numbers to make it unique.
+            int hash = 9;
+            hash = (hash * 7) + this.ID.GetHashCode();
+            hash = (hash * 7) + this.Name.GetHashCode();
+            hash = (hash * 7) + this.Salary.GetHashCode();
+            hash = (hash * 7) + this.Department.GetHashCode();
+            return hash;
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ObjectsCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emp1 = new Employee();
+            var emp2 = new Employee();
+            ///////////////////////////
+            emp1.ID = 1001;
+            emp1.Name = "Ehap Azzazi";
+            emp1.Salary = 3000m;
+            emp1.Department = "Backend Dept.";
+            //////////////////////////
+            emp2.ID = 1001;
+            emp2.Name = "Ehap Azzazi";
+            emp2.Salary = 3000m;
+            emp2.Department = "Backend Dept.";
+            ///////////////////////////
+            Console.WriteLine(emp1.Equals(emp2));
+            Console.WriteLine(emp1 == emp2);
+            Console.WriteLine(emp1 != emp2);
+            Console.ReadKey();
+        }
+    }
+}
